cd30206271619bdf088680861ae204835f7f0f02
Rohit Gohri <gohri.rohit@gmail.com>
2015-07-07 01:49:01 +0530

    Tagging feature added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,8 +5,4 @@ class CommentsController < ApplicationController
     @comment.save
     redirect_to article_path(@comment.article)
   end
-
-  def comment_params
-    params.require(:comment).permit(:author_name, :body)
-  end
 end
