c576f8e2d2a70139d856503c088d1a6dea92d17a
Rohit Gohri <gohri.rohit@gmail.com>
2015-07-07 21:14:03 +0530

    Added caesar cipher solution

---
+++ caesar_cipher.rb
@@ -0,0 +1,28 @@
+def caesar_cipher(text,shift_factor)
+  ans = ""
+  text.each_char { |char| ans += cipher(char, shift_factor)}
+  puts ans
+end
+
+def cipher(char, factor)
+  asc = char.ord
+  if((65..90).include?(asc))
+    asc += factor
+    if (!(65..90).include?(asc))
+      asc = (asc-91) + 65
+    end
+  elsif ((97..122).include?(asc))
+    asc += factor
+    if (!(97..122).include?(asc))
+      asc = (asc-123) + 97
+    end
+  end
+  char = asc.chr
+  return char
+end
+
+puts "Enter text:"
+text = gets.chomp
+puts "Enter shift factor:"
+factor = gets.chomp.to_i
+caesar_cipher(text, factor)
