77e9ddca3c1cb2266ce5c0bbb82a742415bc06b8
Rohit Gohri <gohri.rohit@gmail.com>
2015-07-09 18:16:53 +0530

    Added method bubble_sort_by, accepts block of code to compare

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -12,6 +12,20 @@ def bubble_sort(arr)
   return ans
 end
 
-puts "Enter array to be sorted, seperated by spaces"
-arr = gets.chomp.split.map(&:to_i)
-p bubble_sort(arr)
+def bubble_sort_by(arr)
+  ans = arr
+  j = arr.size-1
+  while j !=0
+    j.times do |i|
+      if ( (yield ans[i], ans[i+1]) < 0)
+        ans[i], ans[i+1] = ans[i+1], ans[i]
+      end
+    end
+    j -=1
+  end
+  return ans
+end
+
+#puts "Enter array to be sorted, seperated by spaces"
+#arr = gets.chomp.split.map(&:to_i)
+#p bubble_sort_by(["hi","hello","hey"]) {|left,right| right.length - left.length }
